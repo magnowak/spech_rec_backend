c5f00ea2cfe34d4728d04f7e183963bace6cb032
Bury <agnieszka.bury@capgemini.com>
2024-01-31 10:29:27 +0100

    fix bug

--- server.js
+++ server.js
@@ -87,7 +87,7 @@ app.post('/api/fillForm', async (req, res) => {
   }
 });
 
-const PORT = 3002;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
